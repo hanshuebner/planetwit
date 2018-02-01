0feac74f17a62ea9d65523bec47c877c14e2cbec
Hans Huebner <hans.huebner@gmail.com>
2018-02-01 12:49:10 +0100

    run in loop

--- run.sh
+++ run.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-sbcl --load load.lisp --eval '(planetwit:main)'
+while true
+do
+    sbcl < /dev/null \
+        --disable-debugger --load load.lisp --eval '(planetwit:main)' --eval '(sb-ext:quit)' \
+        | tee /dev/tty \
+        | mail -s "planetwit crashed" hans.huebner@gmail.com
+    sleep 30
+done
